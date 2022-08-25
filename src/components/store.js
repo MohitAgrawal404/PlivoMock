7f9fea438f354f2ee70799c4f141cccb7d7af788
Mohit Agrawal <mohit@mohits-air.lan>
2022-08-24 21:21:02 -0700

    adding files for setup + Redux for state

---
+++ src/components/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import useReducer from './features/userSlice'
+import chatreducer from './features/chatSlice'
+
+export default configureStore({
+  reducer: {
+    user: useReducer,
+    chat: chatreducer
+  },
+});
+
