5e739c846504f93a61dec9c6c3da60f19cc273f9
Mohit Agrawal <mohit@mohits-air.lan>
2022-08-29 11:37:50 -0700

    message fix working front end

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { selectUser } from './features/userSlice';
+import React, { forwardRef } from 'react';
 import { useSelector } from 'react-redux';
-export const Message = (({ id, data: {timestamp, displayName, email, uid, message}}, ref) => {
-
-    const user = useSelector(selectUser);
-
-
-    return (
-       <div ref = {ref} className = {`message ${user.email === email && "message__sender"}`}>
-            <p>{message}</p>
-            <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
-        </div>
-    )
+import { selectUser } from './features/userSlice';
+ 
+ 
+export const Message = forwardRef(({ id, contents: {timestamp, message, uid, email, displayName}}, ref) => {
+   const user = useSelector(selectUser);
+ 
+ 
+   return (
+      <div ref = {ref} className = {`message ${user.email === email && "message__sender"}`}>
+           <p>{message}</p>
+           <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
+       </div>
+   )
 })
