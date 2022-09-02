bddb90089dc3628f6137f7db464f43bc0fac84bd
Mohit Agrawal <mohit@mohits-air.lan>
2022-09-01 17:33:18 -0700

    customer data

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,16 +1,59 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import { collection } from "firebase/firestore"; 
 import {db} from '../config/firebase';
 import { setChat } from './features/chatSlice';
 import { useDispatch } from 'react-redux'
+import { Message } from './Message';
+import FlipMove from 'react-flip-move';
+import { setDoc, onSnapshot, query, collection, orderBy, doc, addDoc, getDocs } from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import {db} from '../config/firebase';
+import { selectChatId, selectChatName } from './features/chatSlice';
+import { useSelector } from 'react-redux';
+
 
 
 export const Customer = () => {
 
     const dispatch = useDispatch();
     const [active, setActive] = useState(false);
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [change, setChange] = useState(true);
+    const chatId = useSelector(selectChatId); 
 
-    const onClick = () => {
+    useEffect(() => {
+        if(chatId) {
+            (async () => {
+                const q = query(collection(db, 'chats', chatId,'messages'), orderBy("timestamp"));
+                const querySnapshot = await getDocs(q);
+                if (querySnapshot != null) {
+                    let temp = [];
+                    querySnapshot.forEach(
+                    (snapshot) => {
+                        
+                        temp.push({
+                            id: snapshot.id,
+                            data: snapshot.data()
+                        })
+                        
+                    })
+                    setMessages(temp);
+                    console.log(messages);
+                    console.log(chatId);
+                }
+            })();
+            return () => {
+            }  
+        }
+    }, [chatId, change])
+
+
+    const onHandleSubmit = (event) => {
+        event.preventDefault();
+        setActive(true);
         const chatName = "In-app Customer";
 
         
@@ -24,15 +67,68 @@ export const Customer = () => {
                 chatName: chatName
             })
         )
-        }
+    }
 
+    const sendMessage = (e) => {
+        e.preventDefault();
+        addDoc(collection(db, 'chats', chatId,'messages'), {
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: "Anonymous",
+            email: email,
+            displayName: name
+
+        });
+        setInput("");
+        setChange(!change);
+    }
+    
 
     return (
         <div>
             {!active ? 
-            (<div> not able to chat</div>):
+            (<div> 
+                <form onSubmit = {onHandleSubmit}>
+                    <label>
+                        Name: 
+                        <input type = "text" name = "Name" onChange = {(e) => setName(e.target.value)}/>
+                    </label>
+                    <label>
+                        Email: 
+                        <input type = "text" name = "Email" onChange = {(e) => setEmail(e.target.value)}/>
+                    </label>
+                    <input type="submit" />
+
+                </form>
+            </div>
+            )
             
-            (<div> able to chat</div>)
+            :
+            
+            (<div> 
+                <div>
+                    Customer Service
+                </div>
+                <div>
+                    <FlipMove> 
+                        {messages.map(({id, data}) => (
+                            <Message key = {id} contents = {data} />
+                        ))}
+                    </FlipMove>
+                </div>
+                <div className ="chat__input">
+                    <form>
+                        <input 
+                            value = {input}
+                            placeholder = "!message" 
+                            type = "text" 
+                            onChange = {(e) => setInput(e.target.value)}
+                        />
+                        <button onClick = {sendMessage}>Send Message</button>
+                    </form>
+                </div>
+            </div>
+            )
             }
         </div>
     )
