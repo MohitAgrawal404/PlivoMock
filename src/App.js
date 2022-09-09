2c228a4ec9c23f03844f6af06875eb8f6c81e60b
Jekuang3 <jkuang10@gmail.com>
2022-09-09 14:56:34 -0700

    sms message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
-import Login from './components/Login';
-import {Dashboard} from './components/Dashboard';
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout, selectUser } from './components/features/userSlice';
+import React, { useState, useEffect } from "react";
+import Login from "./components/Login";
+import { Dashboard } from "./components/Dashboard";
+import SMS from "./components/SMS";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./components/features/userSlice";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "./config/firebase";
-import Camera from './components/camera.js';
+import Camera from "./components/camera.js";
 import { AuthContextProvider } from "./context/AuthContext";
 import Contact from "./components/Contact";
 import PhotoSend from "./components/fileSending";
@@ -16,32 +17,34 @@ function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
-  useEffect (() => {
-    auth.onAuthStateChanged(authUser => {
-      if (authUser){
-        dispatch(login({
-          uid: authUser.uid,
-          email: authUser.email,
-          displayName: authUser.displayName
-        }))
-      } else{
-        dispatch(logout())
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  
   return (
     <AuthContextProvider>
       <Router>
-        <Navbar/>
-          <Routes>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/camera" element={<Camera/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/fileSending" element={<PhotoSend/>}/>
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/camera" element={<Camera />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/fileSending" element={<PhotoSend />} />
+          <Route path="/sms" element={<SMS />} />
+        </Routes>
       </Router>
     </AuthContextProvider>
   );
---
+++ src/components/SMS.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import "./SMS.css";
+export default function SMS() {
+  const [recipient, setRecipient] = useState("");
+  const [textmessage, settextmessage] = useState("");
+
+  const sendText = (e) => {
+    e.preventDefault();
+    // const recipientInput = document.getElementById("recipient");
+    // const textmessageInput = document.getElementById("textmessage");
+
+    // recipientInput.value = "";
+    // textmessageInput.value = "";
+    Axios.get(
+      `http://localhost:4001/send-text?recipient=${recipient}&textmessage=${textmessage}`
+    ).catch((err) => {
+      console.log(err);
+    });
+  };
+  return (
+    <div className="sms-container">
+      <h3>Send Text Message</h3>
+      <label>Phone Number</label>
+      <input
+        id="recipientInput"
+        onChange={(e) => {
+          setRecipient(e.target.value);
+        }}
+      ></input>
+      <label>Message</label>
+      <textarea
+        id="textmessage"
+        onChange={(e) => settextmessage(e.target.value)}
+      ></textarea>
+      <button onClick={(e) => sendText(e)}>Send Text</button>
+    </div>
+  );
+}
