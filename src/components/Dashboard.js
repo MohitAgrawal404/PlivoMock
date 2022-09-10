c90939c12fd75c72c17fab042f9034649327d922
Mohit Agrawal <mohit@mohits-air.lan>
2022-09-10 16:18:37 -0700

    Adding button for viewing emails on same page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,18 +6,36 @@ import { Container } from 'react-bootstrap';
 import {FullView} from './FullView'
 import { Customer } from './Customer';
 import PhotoSend from './fileSending';
+import {Email} from './Email';
 export const Dashboard = () => {
     const navigate = useNavigate()
     const {user, logOut} = UserAuth()
+    const [selected, setSelected] = useState("message")
+
+
 
     return (
         <div>
             {!user ? 
             (<Customer/>) 
             : 
-            (<div><FullView />
-
-            </div>)
+            (
+            <div> 
+                <button onClick = {() => setSelected("message")}>
+                    In-App Messages
+                </button>
+                <button onClick = {() => setSelected("email")}>
+                    Emails
+                </button>
+                <div>
+                    {selected === "message" ?
+                    (<FullView/>)
+                    :
+                    (<Email/>) 
+                    }
+                </div>
+            </div>
+            ) 
             }
         </div>
     )
